826c7b1ed2d8a3533471e5d07cc07d8d11c570a7
Replit user <>
2024-01-08 20:39:50 +0000

    replit test

--- myApp.js
+++ myApp.js
@@ -1,8 +1,15 @@
 let express = require('express');
 let app = express();
 
+module.exports = { app };
+console.log("Hello world");
 
 
+app.get("/", function(req,res) {
+  res.send("Hello Express");
+});
+
+console.log(app);
 
 
 
